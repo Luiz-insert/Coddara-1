4dbb281452785998ad1e4ff3905e898308a8136e
Lgtt <lgusttavo29@gmail.com>
2025-03-07 00:39:58 -0300

    add @vercel/analytics integration to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
 import HomePage from './components/HomePage'
+import { inject } from '@vercel/analytics'
+
 
 export default function App() {
+  inject()
   return <HomePage />
 }
 
